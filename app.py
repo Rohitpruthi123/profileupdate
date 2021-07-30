c4a005054ce7372d0af7f33f6ebb861bed5711d1
aigym <45813469+aigym@users.noreply.github.com>
2021-07-30 14:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,8 +157,9 @@ def build_topic_model(dataset, column_name,ntopics):
     num_topics = ntopics
     # Build LDA model
     
-    lda_model = gensim.models.LdaModel(corpus=corpus,
-                                           num_topics=num_topics)
+    lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=num_topics,random_state =100, workers = 1)
     # Print the Keyword in the 10 topics
     pprint(lda_model.print_topics())
     doc_lda = lda_model[corpus]
